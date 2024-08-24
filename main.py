3b665e6e14a2ceed6294fbacc7f4d76dd101b0bc
EC2 Default User <ec2-user@ip-172-31-14-185.ap-northeast-1.compute.internal>
2024-08-24 09:03:27 +0000

    fix

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+import uvicorn
+
+key=""
+client = OpenAI(api_key=key)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["0.0.0.0"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.exception_handler(RequestValidationError)
+async def handler(request:Request, exc:RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/result", response_class=HTMLResponse)
+async def GetResult(request:Request, name, blood_type):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "あなたはプロの占い師です。"},
+            {"role": "system", "content": "与えられた情報からユーザーのラッキーカラーを占ってください"},
+            {"role": "system", "content": "出力は、~さんのラッキーカラーは~色です。から始めてください"},
+            {
+                "role": "user",
+                "content": f"名前は{name}で血液型は{blood_type}です。"
+            }
+        ]
+    )
+    return templates.TemplateResponse("result.html", {"request": request, "result": completion.choices[0].message.content})
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
